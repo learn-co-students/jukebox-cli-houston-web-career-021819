891e78eff96d15aaef205ec911fba1d3804351f8
HemalathaMurugan <hema@Hemalathas-MBP.attlocal.net>
2019-03-09 22:03:36 -0600

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,14 +1,14 @@
-songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
+# songs = [
+#   "Phoenix - 1901",
+#   "Tokyo Police Club - Wait Up",
+#   "Sufjan Stevens - Too Much",
+#   "The Naked and the Famous - Young Blood",
+#   "(Far From) Home - Tiga",
+#   "The Cults - Abducted",
+#   "Phoenix - Consolation Prizes",
+#   "Harry Chapin - Cats in the Cradle",
+#   "Amos Lee - Keep It Loose, Keep It Tight"
+# ]
 
 def help 
   help = <<-HELP
@@ -42,10 +42,11 @@ def exit_jukebox
 end
 
 def run(songs)
-  puts "Please enter a command"
-    input = gets.downcase.strip
-    while input 
     
+    input = ""
+    while input 
+      puts "Please enter a command:"
+      input = gets.downcase.strip
     case input
     when 'list'
       list(songs)
@@ -56,6 +57,7 @@ def run(songs)
       play(songs)
     when 'exit'
       exit_jukebox
+      break
     else
       help
     end
